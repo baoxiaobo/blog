57f8984d9c943f74d54d05de988ff61b97b124ca
bxb <674151765@qq.com>
2018-08-04 11:02:48 +0800

    mock data

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+axios.get('/api/personManagement').then(function (res) {
+  console.log(res)
+})
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+</style>
